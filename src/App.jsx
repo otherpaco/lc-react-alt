b0249d2189258471375c7cf15f2509cb31926c4c
Paco Beumer <p.beumer@gmx.net>
2022-10-06 21:44:54 +0200

    Episode 1 - Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Another from './Another';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+    const increment = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Another name="Paco"/>
+      <div>
+        <span>{count}</span>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
