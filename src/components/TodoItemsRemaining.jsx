44bf193d4addac692906e72d482269e9e3e3702b
Paco Beumer <p.beumer@gmx.net>
2022-10-12 08:42:29 +0200

    Episode 8 - Other Built-in React Hooks

--- src/components/TodoItemsRemaining.jsx
+++ src/components/TodoItemsRemaining.jsx
@@ -1,9 +1,14 @@
+import { useMemo } from 'react';
+
 const TodoItemsRemaining = ({ todos }) => {
-  const remainingTodos = () => {
+  const calcRemainingTodos = () => {
+    console.log('remaining Todos fct called');
     return todos.filter((todo) => !todo.isComplete).length;
   };
 
-  return <span>{remainingTodos()} items remaining</span>;
+  const remainingTodos = useMemo(calcRemainingTodos, [todos]);
+
+  return <span>{remainingTodos} items remaining</span>;
 };
 
 export { TodoItemsRemaining };
